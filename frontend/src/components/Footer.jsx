7db522a2edf4dcc492e748ccb54a7c38eee856ad
Anand Kumar <kumaranandstudent@gmail.com>
2025-03-30 19:06:29 +0530

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,11 +7,10 @@ const Footer = () => {
   return (
     <footer className="border-t">
       <div className="container mx-auto p-4 text-center flex flex-col lg:flex-row lg:justify-between gap-2">
-        <p>
-          © All Rights Reserved{" "}
+        <p> © All Rights Reserved
           {new Date().toLocaleString("default", { year: "numeric" })}.
         </p>
-
+        {/* Icon of facebook, instagram, linkedin */}
         <div className="flex items-center gap-4 justify-center text-2xl">
           <a href="" className="hover:text-primary-100">
             <FaFacebook />
